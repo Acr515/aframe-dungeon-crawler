4c20bbec477aabdb2239d8f3b6b3f5f7794fd1ff
Alex Rummel <rummelalexander@outlook.com>
2021-06-01 18:06:43 -0400

    Fixed syntax error

--- components/ControllerRight.js
+++ components/ControllerRight.js
@@ -13,9 +13,9 @@ AFRAME.registerComponent('controller-right', {
             Global.height = 0;
         });
 
-        this.el.addEventListener('triggerup'), function (evt) {
+        this.el.addEventListener('triggerup', function (evt) {
             if (this.activeTile == null) return;
             get("rig").object3D.position.set(this.activeTile.object3D.position.x * Global.CONSTANTS.areaScale, Global.height, this.activeTile.object3D.position.z * Global.CONSTANTS.areaScale);
-        }
+        });
     }
 });
