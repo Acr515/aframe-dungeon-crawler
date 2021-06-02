86f1f308908843760c33987124f20c1a12a9a8a3
Alex Rummel <rummelalexander@outlook.com>
2021-06-01 23:37:49 -0400

    Toying with position vars

--- components/ControllerRight.js
+++ components/ControllerRight.js
@@ -30,8 +30,8 @@ AFRAME.registerComponent('controller-right', {
             
             processTurn({
                 x: this.activeTile.object3D.position.x * Global.CONSTANTS.areaScale,
-                y: Global.height,
-                z: this.activeTile.object3D.position.z * Global.CONSTANTS.areaScale
+                y: this.activeTile.object3D.position.y * Global.CONSTANTS.areaScale,
+                z: Global.height
             });
         });
     }
