c5b06ea39961a88f8dab5a2361c39326dd843567
Alex Rummel <rummelalexander@outlook.com>
2021-06-01 23:34:09 -0400

    Misspelled

--- components/Tile.js
+++ components/Tile.js
@@ -23,12 +23,12 @@ AFRAME.registerComponent('tile', {
 
 		// Laser pointer now focused on this element
 		this.el.addEventListener('raycaster-intersected', evt => {
-			get("controller-right").emit("update-tile", { tile: evt.detail.el });
+			get("controllerRight").emit("update-tile", { tile: evt.detail.el });
 			this.el.setAttribute("material", "color", "#999999");
 		});
 		// Laser pointer now looking away from this element
 		this.el.addEventListener('raycaster-intersected-cleared', evt => {
-			get("controller-right").emit("update-tile", { tile: null })
+			get("controllerRight").emit("update-tile", { tile: null })
 			this.el.setAttribute("material", "color", this.hex);
 		});
 	},
